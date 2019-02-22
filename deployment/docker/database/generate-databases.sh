359d29d8a80be7e7bdd21df9d96bcfe3d36e0a71
NlxF <luxiaofei911@gmail.com>
2019-02-22 18:01:51 +0800

    update database config

---
+++ deployment/docker/database/generate-databases.sh
@@ -0,0 +1,17 @@
+# TODO: remove this line after we fully support mysql 5.7
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "SET GLOBAL sql_mode = '';"
+
+# TODO: remove this line after we squash our DB updates
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "SET GLOBAL max_allowed_packet=128*1024*1024;"
+
+echo "Creating DB..."
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS wuaiwow"
+
+#if [ -f ./thirdParty/thirdParty_build.sh ]; then
+#    echo "Importing wuaiwow base..."
+#    mysql -u root -p$MYSQL_ROOT_PASSWORD wuaiwow < /sql/wuaiwow_base.sql
+#fi
+
+#mysqldump -u root -p --add-drop-table --all-databases --force > data-for-upgrade.sql
+
+echo "Done!"
